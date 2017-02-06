a4692b77640b9fb15ec5b57991f002cdd922f891
Matheus Link <matheuslink1996@gmail.com>
2017-02-06 12:21:46 -0200

    Finished

--- js/app/views/NegociacoesView.js
+++ js/app/views/NegociacoesView.js
@@ -1,10 +1,10 @@
-class NegociacoesView {
-    
+class NegociacoesView extends View {
+
     constructor(elemento) {
-        this._elemento = elemento;''
+        super(elemento);
     }
 
-    _template(model) {
+    template(model) {
         return `
             <table class="table table-hover table-bordered">
                 <thead>
@@ -17,9 +17,8 @@ class NegociacoesView {
                 </thead>
                 
                 <tbody>
-                    ${model.negociacoes.map(n => {
-
-                        return `
+                    ${model.negociacoes.map(n => 
+                       `
                             <tr>
                                 <td>${DateHelper.dataParaTexto(n.data)}</td>
                                 <td>${n.quantidade}</td>
@@ -27,16 +26,14 @@ class NegociacoesView {
                                 <td>${n.volume}</td>
                             </tr>
                         `
-                    }).join('')}
+                    ).join('')}
                 </tbody>
                 
                 <tfoot>
-                </tfoot>
+                    <td colspan="3"></td>
+                    <td>${model.negociacoes.reduce((total, n) => total + n.volume, 0.0)}</td>
+                </tfoot> 
             </table>
         `;
-    }
-
-    update(model) {
-        this._elemento.innerHTML = this._template(model);
     }
 }
